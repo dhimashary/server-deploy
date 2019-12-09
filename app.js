51a28462feaf916163e7ba8a1fbc02684826efac
Hary Dhimas <dhimas.hary@gmail.com>
2019-12-09 11:34:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const cors = require('cors')
-
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/pokemon', (req, res) => {
